df793f409b54f3a850e943390bcbe0568944d336
Udaykiranmallu <udaykiranmallu1620@gmail.com>
2025-12-31 20:36:33 +0530

    Add files via upload

---
+++ Code-27-75.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("The number is not prime");
+        return 0;
+    }
+}
