c33770ea236b4223ebba70d2097fac775ea836d7
Udaykiranmallu <udaykiranmallu1620@gmail.com>
2026-01-09 22:06:31 +0530

    Add files via upload

---
+++ Code-52-75.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[50], rev[50];
+    int i, len;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len; i++) {
+        rev[i] = str[len - i - 1];
+    }
+    rev[i] = '\0';
+
+    if (strcmp(str, rev) == 0) {
+        printf("The string is a palindrome.");
+    } else {
+        printf("The string is not a palindrome.");
+    }
+
+    return 0;
+}
