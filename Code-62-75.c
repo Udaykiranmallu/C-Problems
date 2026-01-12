260461b2f2b39e89d31a95b4756968842c543d44
Udaykiranmallu <udaykiranmallu1620@gmail.com>
2026-01-12 19:53:58 +0530

    Add files via upload

---
+++ Code-62-75.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+    int sum = 0;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            sum = sum + i;
+        }
+    }
+
+    printf("Sum of all prime numbers up to %d = %d", n, sum);
+
+    return 0;
+}
