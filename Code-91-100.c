dd60a2e429f3a81370ea25446c4df7ee88a32be1
Udaykiranmallu <udaykiranmallu1620@gmail.com>
2026-01-28 20:46:45 +0530

    Add files via upload

---
+++ Code-91-100.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int calculateBill(int units)
+{
+    int bill = 0;
+
+    if (units <= 100)
+    {
+        bill = units * 5;
+    }
+    else if (units <= 200)
+    {
+        bill = (100 * 5) + (units - 100) * 7;
+    }
+    else
+    {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 8;
+    }
+
+    return bill;
+}
+
+int main()
+{
+    int units, amount;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    amount = calculateBill(units);
+
+    printf("Total Bill Amount: %d\n", amount);
+
+    return 0;
+}
